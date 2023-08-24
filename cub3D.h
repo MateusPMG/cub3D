644220f5559111af331d0e0b0ae7d32123bccab2
MateusPMG <mpmgboss@gmail.com>
2023-08-24 16:30:49 +0100

    Starting parsing

---
+++ cub3D.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 15:39:48 by mpatrao           #+#    #+#             */
+/*   Updated: 2023/08/24 16:26:19 by mpatrao          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+typedef struct s_data
+{
+	char	**map;
+	int		floor;
+	int		ceiling;
+	char	texture[4];
+
+}	t_data;
+
+#endif
