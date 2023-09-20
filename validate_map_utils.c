a7126c5818f965476e18be21d0696f99632cd809
MateusPMG <mpmgboss@gmail.com>
2023-09-20 15:17:10 +0100

    finished base structure?

--- validate_map_utils.c
+++ validate_map_utils.c
@@ -6,7 +6,7 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 16:08:33 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/09/19 17:17:52 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/09/20 15:13:52 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int	walkable(char c)
 {
-	
+	if (c == 'N' || c == 'S' || c == 'W' || c == 'E' || c == '0')
+		return (1);
+	return (0);
 }
 
 int	check_walls(t_data *data, int j, int i)
@@ -25,7 +27,7 @@ int	check_walls(t_data *data, int j, int i)
 	int	d;
 
 	a = i;
-	while (a > 0 && data->map[++a][j] && walkable(data->map[a][j]))
+	while (a > 0 && (a < data->map_y - 1 && data->map[++a][j]) && walkable(data->map[a][j]))
 		continue ;
 	b = i;
 	while (b > 0 && data->map[--b][j] && walkable(data->map[b][j]))
@@ -36,7 +38,9 @@ int	check_walls(t_data *data, int j, int i)
 	d = j;
 	while (d > 0 && data->map[i][--d] && walkable(data->map[i][d]))
 		continue ;
-	if ()
+	if (!data->map[i][d] || !data->map[i][c] || !data->map[b][j] || !data->map[a][j]
+		|| data->map[i][d] != '1' || data->map[i][c] != '1' || data->map[b][j] != '1'
+		|| (a == data->map_y - 1 && data->map[a][j] != '1'))
 		return (1);
 	return (0);
 }
